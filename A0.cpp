c8e92f42188243dd35bfd491708c305d3dde20ff
Vladimir <39743965+vokzvokz@users.noreply.github.com>
2021-12-23 23:59:06 +0300

    A0 probably done

--- A0.cpp
+++ A0.cpp
@@ -1,53 +1,58 @@
-//
-// Created by vladimir on 23.12.2021.
-//
-
 #include <string>
 #include <iostream>
-#include <sstream>
-#include <iterator>
 #include <fstream>
 
 using namespace std;
 
-unsigned int countWordsInString(std::string const& str)
-{
-    stringstream stream(str);
-    return distance(istream_iterator<string>(stream), istream_iterator<std::string>());
-}
-
-string read_file(string path){
+string read_file(const string& path){
     std::ifstream t;
     t.open(path);
     std::string buffer;
     std::string line;
     while(t){
         std::getline(t, line);
-        buffer = buffer+line;
+        buffer += "\n"+line;
     }
     t.close();
     return buffer;
 }
 
 int count_words(string s){
-//    cout<<s<<endl;
-    return s.max_size();
+    int i = 0;
+    int counter = 0;
+    cout<<"START COUNTING WORDS IN LINE: "<<s<<endl;
+    while(s[i]!='\0'){
+        while(s[i]==' ' || s[i]=='\t' || s[i]=='\n'){
+            i++;
+            if(s[i]=='\0') return counter;
+        }
+        counter++;
+        while(s[i]!=' ' && s[i]!='\t' && s[i]!='\n'){
+            i++;
+            if(s[i]=='\0') return counter;
+        }
+    }
+    return counter;
 }
 
-int main(int argc, char * argv[]){
+int main(int argc, char* argv[]){
 //    string s = argv[1];
 //    cout<<countWordsInString(s)<<endl;
-
+//    cout<<argc<<" "<<argv[0]<<" "<<argv[1]<<endl;
     string s;
 //    cout<<argc<<" "<<argv[1]<<" "<<string(argv[2])<<" "<<(string(argv[2]) == "f")<<endl;
-    if(string(argv[2])=="f"){
+    if(argc == 2){
+        s = argv[1];
+    }
+    else if(string(argv[2])=="f"){
         cout<<"READING FROM FILE"<<endl;
         s = read_file(argv[1]);
     }
     else{
-        s = argv[1];
+        cout<<"Invalid nubmer of arguments"<<endl;
+        return -1;
     }
-    cout<<countWordsInString(s)<<endl;
+    cout<<count_words(s)<<endl;
 
     return 0;
 }
